a2dc10ba848126bc9b36499eb2d65f8cd9cb603e
philipheferreira <philipheferreira@gmail.com>
2024-05-27 11:33:40 -0300

    upgrade: chage 13 for class and querySelector

--- 13 - Manipulação de Strings/script.js
+++ 13 - Manipulação de Strings/script.js
@@ -1,10 +1,10 @@
 function diminuirNome(){
-	var s = document.getElementById("t").innerHTML; // Selecionar template com base no id e colocar em uma variavel 
+	var s = document.querySelector(".t").innerHTML; // Selecionar template com base no id e colocar em uma variavel 
 	var r = s.toLowerCase(); // pegar valor da variavel e diminuida pra colocar em outra variavel
-	document.getElementById("t").innerHTML = r; // retornar a variavel diminuida
+	document.querySelector(".t").innerHTML = r; // retornar a variavel diminuida
 }
 function aumentarNome(){
-	var s = document.getElementById("t").innerHTML; // Selecionar template com base no id e colocar em uma variavel
+	var s = document.querySelector(".t").innerHTML; // Selecionar template com base no id e colocar em uma variavel
 	var r = s.toUpperCase(); // pegar valor da variavel e aumentar pra colocar em outra variavel
-	document.getElementById("t").innerHTML = r; // retornar a variavel aumentada
+	document.querySelector(".t").innerHTML = r; // retornar a variavel aumentada
 }
